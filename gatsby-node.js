11574234c9951c489a6453a28609a9de4e07c1d0
David Weirich <davidweirich@davids-mbp-2.lan>
2018-07-20 16:31:21 -0700

    Typo fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
   if (stage === "build-html") {
     config.loader("null", {
       test: /react-leaflet/,
-      loader: path.resolve(__dirname, './loader.js'),,
+      loader: path.resolve(__dirname, './loader.js'),
     });
   }
 };
