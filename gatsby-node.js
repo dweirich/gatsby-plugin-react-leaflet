0f97a3c627fddb09719628b285a3ade7ed1b94ec
David Weirich <davidweirich@davids-mbp-2.lan>
2018-07-20 21:14:06 -0700

    Gatsby v1 fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-const path = require('path')
-
 exports.modifyWebpackConfig = ({ config, stage }) => {
   if (stage === "build-html") {
     config.loader("null", {
       test: /react-leaflet/,
-      loader: path.resolve(__dirname, './loader.js'),
+      loader: "null-loader",
     });
-=======
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /react-leaflet/,
-            use: loaders.null(),
-          }
-        ]
-      }
-    })
->>>>>>> e559051fa94c7b7474ff1320624044a1f82e01e6
   }
 }
