0ef380165b87cb8634dd04d1735ae7edac8d5f29
David Weirich <davidweirich@davids-mbp-2.lan>
2018-12-21 08:24:05 -0800

    Fix crossorigin error

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,9 +4,9 @@ exports.onRenderBody = ({ setHeadComponents }) => {
   const link = React.createElement('link', {
     key: 'leaflet',
     rel: 'stylesheet',
-    href: 'https://unpkg.com/leaflet@1.3.3/dist/leaflet.css',
+    href: 'https://unpkg.com/leaflet@1.3.4/dist/leaflet.css',
     integrity: 'sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==',
-    crossorigin: ''
+    crossOrigin: ''
   })
 
   setHeadComponents([
