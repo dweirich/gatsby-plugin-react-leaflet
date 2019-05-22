d9f51037d4d1b56478a4b390f8317aa877c3e23f
David Weirich <davidweirich@davids-mbp-2.lan>
2019-05-22 13:04:14 -0700

    Remove attempt at loading markers

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,22 +6,11 @@ exports.onCreateWebpackConfig = function (_ref) {
   var stage = _ref.stage,
       loaders = _ref.loaders,
       actions = _ref.actions;
-  var config = {
-    module: {
-      rules: [{
-        test: /\.css$/,
-        include: [path.resolve('leaflet/dist')],
-        use: [loaders.style(), loaders.css()]
-      }]
-    }
-  };
 
   if (stage === "build-html") {
-    config.module.rules = [].concat(config.module.rules, [{
+    actions.setWebpackConfig({
       test: /\/node_modules\/react-leaflet|\/node_modules\/leaflet/,
       use: loaders["null"]()
-    }]);
+    });
   }
-
-  actions.setWebpackConfig(config);
 };
