6f3cd48ed9234c69223bca2ae0c543ddf09235a9
Stephan Meijer <stephan.meijer@gmail.com>
2020-04-13 14:50:28 +0200

    feat: fix loading of leaflet in `develop` mode
    fixes #17

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
+  if (stage === "build-html" || stage === "develop-html") {
     const regex = [
       /node_modules\/leaflet/,
       /node_modules\\leaflet/
