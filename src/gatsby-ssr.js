01c95ba0e90901baecd4343f32dc429ae7b8f99c
David Weirich <david@dweirich.com>
2022-11-27 15:18:53 -0800

    Switch integrity key from Javascript file to stylesheet file

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -7,7 +7,7 @@ exports.onRenderBody = ({ setHeadComponents }, options) => {
     key: 'leaflet',
     rel: 'stylesheet',
     href: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
-    integrity: 'sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=',
+    integrity: 'sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=',
     crossOrigin: ''
   })
 
